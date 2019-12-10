80538e0e464930768e4e31927a1aec76a93dafdf
etefia60994 <etefia60994@gmail.com>
2019-12-09 19:31:14 -0600

    Fixed?

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs_oo.Tests;
+
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
@@ -8,54 +10,53 @@ import java.util.prefs.BackingStoreException;
 
 
 public class JobTest {
-    Job job1;
-    Job job2;
-    Job ProductTestee;
-    Job ProductTester;
-    Job BlankField;
-    @Before
-    public void generateJobs(){
-        job1 = new Job();
-        job2 = new Job();
-        ProductTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        ProductTestee = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        BlankField = new Job("position", new Employer(""), new Location ("St. Elsewhere"), new PositionType("Wage Slave"), new CoreCompetency("C#"));
-    }
+
+    Job job1 = new Job();
+    Job job2 = new Job();
+    Job ProductTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    Job ProductTestee = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    Job BlankField = new Job("position", new Employer(""), new Location("St. Elsewhere"), new PositionType("Wage Slave"), new CoreCompetency("C#"));
+
     @Test
-    public void testSettingJobId(){
+    public void testSettingJobId() {
         assertTrue(job2.getId() - job1.getId() == 1 && job1.getId() != job2.getId());
-        assertEquals( job1.getId(), 1);
+        assertEquals(job1.getId(), 1);
         assertEquals(job2.getId(), 2);
     }
-@Test
-    public void testJobConstructor(){
-    assertTrue(ProductTester.getEmployer() instanceof Employer);
-    assertTrue(ProductTester.getLocation() instanceof Location);
-    assertTrue(ProductTester.getPositionType() instanceof PositionType);
-    assertTrue(ProductTester.getCoreCompetency() instanceof CoreCompetency);
 
-}
-@Test
-    public void testJobsforEquality(){
+    @Test
+    public void testJobConstructor() {
+        assertTrue(ProductTester.getEmployer() instanceof Employer);
+        assertTrue(ProductTester.getLocation() instanceof Location);
+        assertTrue(ProductTester.getPositionType() instanceof PositionType);
+        assertTrue(ProductTester.getCoreCompetency() instanceof CoreCompetency);
+
+    }
+
+    @Test
+    public void testJobsforEquality() {
         assertFalse(ProductTester.equals(ProductTestee));
-}
-@Test
+    }
+
+    @Test
     public void toStringTestNewLines() {
-    assertTrue(ProductTester.toString().startsWith("\n"));
-    assertTrue(ProductTester.toString().endsWith("\n"));
-}
-@Test
-        public void toStringTestMethod(){
-    assertEquals(ProductTester.toString(),
-            "\n" + "ID: " + ProductTester.getId() +
-                    "\n" + "Name: " + ProductTester.getName() +
-                    "\n" + "Employer: " + ProductTester.getEmployer() +
-                    "\n" + "Location: " + ProductTester.getLocation() +
-                    "\n" + "Position Type: " + ProductTester.getPositionType() +
-                    "\n" + "Core Competency: " + ProductTester.getCoreCompetency() + "\n");
-}
-@Test
-    public void blankFieldTest(){
+        assertTrue(ProductTester.toString().startsWith("\n"));
+        assertTrue(ProductTester.toString().endsWith("\n"));
+    }
+
+    @Test
+    public void toStringTestMethod() {
+        assertEquals(ProductTester.toString(),
+                "\n" + "ID: " + ProductTester.getId() +
+                        "\n" + "Name: " + ProductTester.getName() +
+                        "\n" + "Employer: " + ProductTester.getEmployer() +
+                        "\n" + "Location: " + ProductTester.getLocation() +
+                        "\n" + "Position Type: " + ProductTester.getPositionType() +
+                        "\n" + "Core Competency: " + ProductTester.getCoreCompetency() + "\n");
+    }
+
+    @Test
+    public void blankFieldTest() {
         assertEquals(BlankField.getEmployer().toString(), "No Data Available");
-}
+    }
 }
